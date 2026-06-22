0deefc9f95adf637c20402ce387e9e70ce7919df
SMani24 <mmanimirshabani24@gmail.com>
2026-06-22 22:16:28 +0330

    Add InputHandler

---
+++ src/InputHandler.hpp
@@ -0,0 +1,16 @@
+/*
+ * Naming Convention Guideline
+ * Class names: PascalCase
+ * Function names: camelCase
+ * Variable names: camelCase
+ * Constant names: UPPER_SNAKE_CASE
+ */
+#pragma once
+#include <string>
+
+class InputHandler {
+public:
+    static int readInt(int min, int max);
+    
+    static std::string readLine();
+};
